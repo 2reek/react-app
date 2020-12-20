d349f6471394ac6eaa3271c445bc8f5ae92ff752
2reek <mountagest@gmail.com>
2020-12-20 21:19:08 +0300

    add: swar api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("=====!!!=====")
+const getResource = async(url) => {
+    const res = await fetch(url)
+    const body = await res.json()
+    return body
+}
+
+getResource('https://swapi.dev/api/people/1/').then((body) => {
+    console.log(body);
+    console.log('finished')
+})
+
