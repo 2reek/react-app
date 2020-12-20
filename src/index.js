c53b3daa3ad7f1f9d02151e55ad3c231560328fe
2reek <mountagest@gmail.com>
2020-12-20 22:11:23 +0300

    add: class SwapySwrvice

--- src/index.js
+++ src/index.js
@@ -1,20 +1,74 @@
-const getResource = async (url) => {
-	const res = await fetch(url);
+class SwapyService {
+	_apiBase = 'https://swapi.dev/api';
 
-	if (!res.ok) {
-		throw new Error(`'Could not fetch'===> ${url} 
-        received ===> ${res.status}`);
+	async getResource(url) {
+		const res = await fetch(`${this._apiBase}${url}`);
+
+		if (!res.ok) {
+			throw new Error(`'Could not fetch'===> ${this._apiBase}${url}  received ===> ${res.status}`);
+		}
+
+		return await res.json();
 	}
 
-	const body = await res.json();
-	return body;
-};
+	async getAllPeople() {
+		const res = await this.getResource(`/people/`);
+		return res.results;
+	}
 
-getResource('https://swapi.dev/api/people/111/')
-	.then((body) => {
-		console.log(body);
-		console.log('finished');
-	})
-	.catch((err) => {
-		console.error('Could not fetch', err);
-	});
+	getPerson(id) {
+		return this.getResource(`/people/${id}`);
+	}
+
+	async getAllPlanets() {
+		const res = await this.getResource(`/planets/`);
+		return res.results;
+	}
+
+	getPlanet(id) {
+		return this.getResource(`/planets/${id}`);
+	}
+
+	async getAllStarships() {
+		const res = await this.getResource(`/starships/`);
+		return res.results;
+	}
+
+	getStarship(id) {
+		return this.getResource(`/starships/${id}`);
+	}
+}
+
+//Test
+
+const swapi = new SwapyService();
+
+swapi.getAllPeople().then((body) => {
+	const names = body.map((person) => person.name);
+	console.log(body);
+	console.log(names);
+});
+
+swapi.getPerson(3).then((body) => {
+	console.log(body.name);
+});
+
+swapi.getAllPlanets().then((body) => {
+	const names = body.map((planet) => planet.name);
+	console.log(body);
+	console.log(names);
+});
+
+swapi.getPlanet(3).then((body) => {
+	console.log(body.name);
+});
+
+swapi.getAllStarships().then((body) => {
+	const names = body.map((starship) => starship.name);
+	console.log(body);
+	console.log(names);
+});
+
+swapi.getStarship(3).then((body) => {
+	console.log(body.name);
+});
