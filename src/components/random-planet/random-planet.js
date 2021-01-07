57017c9811f1256776ff7151eac5987c5526a876
2reek <mountagest@gmail.com>
2021-01-07 20:26:21 +0300

    add: toggle button

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -32,7 +32,7 @@ export default class RandomPlanet extends Component {
 	};
 
 	updatePlanet() {
-		const id = Math.floor(Math.random() * 25) + 2;
+		const id = Math.floor(Math.random() * 25) + 3;
 		this.swapiService.getPlanet(id).then(this.onPlanetLoaded).catch(this.onErorr);
 	}
 
